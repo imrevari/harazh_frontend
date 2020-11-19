a3c77439a0e8bba04969be62c5510b65a04cc9df
Imre <i13@bk.ru>
2020-11-19 13:05:54 +0100

    build map added

---
+++ build/precache-manifest.31f7f2f2ac87b9b3acbd196606109236.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2491f3cae0a14c2d5483f2365f7750d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f46b6e52a0be81139601",
+    "url": "/static/css/main.d756b95a.chunk.css"
+  },
+  {
+    "revision": "1e90f83094d0f813cc12",
+    "url": "/static/js/2.af4075e7.chunk.js"
+  },
+  {
+    "revision": "f46b6e52a0be81139601",
+    "url": "/static/js/main.00623e70.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
